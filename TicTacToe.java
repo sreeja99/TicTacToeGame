597282ac19f2ed07857c4a7ae2f2d9c6695e2f15
sreeja99 <srijagodishala@gmail.com>
2020-10-05 17:45:08 +0530

    Refactored :Added A Method To Decide who Plays First

--- TicTacToe.java
+++ TicTacToe.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 
 public class TicTacToe {
 	//constants
+	private final static int TAIL = 0;
+	private final static int HEAD = 1;
+	//variables
 	public static char user;
 	public static char computer;
 	public static char turn;
@@ -31,10 +34,10 @@ public class TicTacToe {
 		System.out.println(board[7]+" | "+board[8]+" | "+board[9]);
 	}
 	// Move to desired location
-		private static int moveLocation(char[] board) {
-			Scanner sc = new Scanner(System.in);
-			Integer[] values = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-			while (true) {
+	private static int moveLocation(char[] board) {
+		Scanner sc = new Scanner(System.in);
+		Integer[] values = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		while (true) {
 				System.out.println("Enter the number between 1 and 9");
 				int location = sc.nextInt();
 				if (Arrays.asList(values).contains(location) && isSpaceFree(board, location)) {
@@ -44,24 +47,40 @@ public class TicTacToe {
 
 		}
         //checking if the location is free
-		private static boolean isSpaceFree(char[] board, int location) {
-			return board[location] == ' ';
+	private static boolean isSpaceFree(char[] board, int location) {
+		return board[location] == ' ';
 		}
 		//changing on board
-		private static void makeMove(char[] board,char turn) {
-			if(turn==user) {
-				int location=moveLocation(board);
-				board[location]=user;
+	private static void makeMove(char[] board,char turn) {
+		if(turn==user) {
+			int location=moveLocation(board);
+			board[location]=user;
 			}
-			else {
-				int location=(int) (Math.floor((Math.random() * 10) % 9) + 1);
-				if(isSpaceFree(board,location)==true) {
+		else {
+			int location=(int) (Math.floor((Math.random() * 10) % 9) + 1);
+			if(isSpaceFree(board,location)==true) {
 					board[location]=turn;
 				}
 			}
 			displayBoard(board);
 		}
-	public static void main(String[] args) {
+	//toss
+	private static int toss() {
+		int toss=(int) (Math.floor(Math.random() * 10) % 2);
+		return toss;
+	}
+	//checking who plays first
+	private static void whoPlaysFirst(int toss,char[] board,char user,char computer) {
+		if(toss==HEAD) {
+			System.out.println("user plays first");
+			makeMove(board,user);
+		}
+		else if(toss==TAIL) {
+			System.out.println("computer starts the game");
+			makeMove(board,computer);
+		}
+	}
+	 public static void main(String[] args) {
 		System.out.println("Welcome to TicTacToe program");
         TicTacToe ticTacToe = new TicTacToe();
 		char[] board = ticTacToe.createBoard();
