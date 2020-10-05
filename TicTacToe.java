95cb457a40f585b50a2317badc9605ae14896acd
sreeja99 <srijagodishala@gmail.com>
2020-10-05 16:52:21 +0530

    Refactored:Choosing a Letter From The User And Assigning The Computer Letter

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,10 +1,36 @@
 package com.tictactoe;
+import java.util.Scanner;
 
 public class TicTacToe {
-
+	//constants
+	public static char user;
+	public static char computer;
+	// Create board with Empty Spaces
+	private char[] createBoard() {
+		char[] board = new char[10];
+			for (int i = 1; i < 10; i++) {
+				board[i] = ' ';
+				}
+				return board;
+			}
+	//choosing letter
+	private static char chooseLetter() {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the Letter");
+		return sc.next().charAt(0);
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Welcome to TicTacToe program");
+        TicTacToe ticTacToe = new TicTacToe();
+		char[] board = ticTacToe.createBoard();
+		char user=ticTacToe.chooseLetter();
+		//assigning computer letter
+		if(user=='X') {
+			computer='O';
+		}
+		else {
+			computer='X';
+		}
 	}
 
 }
