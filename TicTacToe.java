937034e4655247db360105ec0d7f491fbb3f47c6
sreeja99 <srijagodishala@gmail.com>
2020-10-05 17:20:10 +0530

    Refactored:Created A Method For Asking The Desired Locaction from The User

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,10 +1,13 @@
 package com.tictactoe;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class TicTacToe {
 	//constants
 	public static char user;
 	public static char computer;
+	public static char turn;
+	public static char board;
 	// Create board with Empty Spaces
 	private char[] createBoard() {
 		char[] board = new char[10];
@@ -27,6 +30,23 @@ public class TicTacToe {
 		System.out.println("______________");
 		System.out.println(board[7]+" | "+board[8]+" | "+board[9]);
 	}
+	// Move to desired location
+		private static int moveLocation(char[] board,char user) {
+			Scanner sc = new Scanner(System.in);
+			Integer[] values = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+			while (true) {
+				System.out.println("Enter the number between 1 and 9");
+				int location = sc.nextInt();
+				if (Arrays.asList(values).contains(location) && isSpaceFree(board, location)) {
+					return location;
+				}
+			}
+
+		}
+
+		private static boolean isSpaceFree(char[] board, int location) {
+			return board[location] == ' ';
+		}
 	public static void main(String[] args) {
 		System.out.println("Welcome to TicTacToe program");
         TicTacToe ticTacToe = new TicTacToe();
@@ -40,6 +60,7 @@ public class TicTacToe {
 			computer='X';
 		}
 		displayBoard(board);
+		moveLocation(board,user);
 	}
 
 }
